5a6f634b0c3804f4853acb103ca8a4524d6a9c13
Hanlon Miller <hanlonmiller@gmail.com>
2015-02-22 21:45:17 -0500

    Initial Commit

---
+++ TronLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TronLock
+//
+//  Created by Hanlon Miller on 2/22/15.
+//  Copyright (c) 2015 Hanlon Miller Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TronLock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TronLock
+//
+//  Created by Hanlon Miller on 2/22/15.
+//  Copyright (c) 2015 Hanlon Miller Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
